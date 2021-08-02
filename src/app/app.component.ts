a1499fcc85c103a57a5c20fb26955b379a5c1dff
Philip <behrens@blue-it.nrw>
2021-08-02 15:17:32 +0200

    bug test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dx-scheduler-bug-test';
+  appointments: any[] = [];
+
+  constructor() {
+    this.setAppointments(true);
+  }
+
+  schedulerUsers = [
+    {
+      text: 'User 1',
+      id: 1
+    },{
+      text: 'User 2',
+      id: 2
+    }
+  ]
+
+  setAppointments(i: boolean | undefined) {
+    let appointments: any[] = [
+      {
+        text: 'Test 1',
+        startDate: new Date("2021-08-02T09:00:00.000Z"),
+        endDate: new Date("2021-08-02T10:00:00.000Z"),
+        user: 1,
+        allDay: false
+      },
+      {
+        text: 'Test 2',
+        startDate: new Date("2021-08-02T13:00:00.000Z"),
+        endDate: new Date("2021-08-02T14:00:00.000Z"),
+        user: 1,
+        allDay: false
+      }
+    ];
+
+    if(i) {
+      appointments.push({
+          text: 'Test All Day',
+          startDate: new Date("2021-08-02T00:00:00.000Z"),
+          endDate: new Date("2021-08-02T20:00:00.000Z"),
+          allDay: true,
+          user: 1
+        });
+    } else {
+      appointments.push({
+        text: 'Test Not All Day',
+        startDate: new Date("2021-08-02T00:00:00.000Z"),
+        endDate: new Date("2021-08-02T20:00:00.000Z"),
+        allDay: false,
+        user: 1
+      });
+    }
+
+    this.appointments = appointments;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {DxCheckBoxModule, DxSchedulerModule} from "devextreme-angular";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxSchedulerModule,
+    DxCheckBoxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
